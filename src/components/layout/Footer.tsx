92c35dd659ed084310708402b510367be9a3496e
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2024-07-20 14:30:58 +0300

    create footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="bg-slate-50 p-4  mt-12">
+      <div className="container">
+        <p className="text-center text-slate-600">
+          Copyright 2024 &copy; Mahmoud Dahdouh
+        </p>
+      </div>
+    </footer>
+  )
+}
