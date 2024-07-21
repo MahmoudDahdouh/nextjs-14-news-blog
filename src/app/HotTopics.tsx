f55c897096e28c9d5526e4e752eb608b594fba8e
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2024-07-21 11:22:13 +0300

    get home page data

--- src/app/HotTopics.tsx
+++ src/app/HotTopics.tsx
@@ -14,36 +14,40 @@ import {
 } from "lucide-react"
 
 export const HotTopics = [
-  { name: "World", slug: "world", icon: <Globe className="h-6 w-6" /> },
+  { name: "World", slug: "world", icon: <Globe className="w-full h-full" /> },
   {
     name: "Politics",
     slug: "politics",
-    icon: <Briefcase className="h-6 w-6" />,
+    icon: <Briefcase className="w-full h-full" />,
   },
   {
     name: "Science",
     slug: "science",
-    icon: <Microscope className="h-6 w-6" />,
+    icon: <Microscope className="w-full h-full" />,
   },
-  { name: "Sports", slug: "sports", icon: <Medal className="h-6 w-6" /> },
+  { name: "Sports", slug: "sports", icon: <Medal className="w-full h-full" /> },
   {
     name: "Technology",
     slug: "technology",
-    icon: <Cpu className="h-6 w-6" />,
+    icon: <Cpu className="w-full h-full" />,
   },
-  { name: "Health", slug: "health", icon: <Heart className="h-6 w-6" /> },
+  { name: "Health", slug: "health", icon: <Heart className="w-full h-full" /> },
   {
     name: "Entertainment",
     slug: "entertainment",
-    icon: <Film className="h-6 w-6" />,
+    icon: <Film className="w-full h-full" />,
   },
-  { name: "Travel", slug: "travel", icon: <Plane className="h-6 w-6" /> },
+  { name: "Travel", slug: "travel", icon: <Plane className="w-full h-full" /> },
   {
     name: "Business",
     slug: "business",
-    icon: <DollarSign className="h-6 w-6" />,
+    icon: <DollarSign className="w-full h-full" />,
   },
-  { name: "Fashion", slug: "fashion", icon: <Shirt className="h-6 w-6" /> },
-  { name: "Food", slug: "food", icon: <Apple className="h-6 w-6" /> },
-  { name: "Music", slug: "music", icon: <Music className="h-6 w-6" /> },
+  {
+    name: "Fashion",
+    slug: "fashion",
+    icon: <Shirt className="w-full h-full" />,
+  },
+  { name: "Food", slug: "food", icon: <Apple className="w-full h-full" /> },
+  { name: "Music", slug: "music", icon: <Music className="w-full h-full" /> },
 ]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,63 +2,98 @@ import Image from "next/image"
 import { HotTopics } from "./HotTopics"
 import { User } from "lucide-react"
 import Link from "next/link"
+import Axios from "@/config/axios"
+import BrokenImage from "@/components/BotkenImage"
 
-export default function Home() {
+async function getHomeData() {
+  try {
+    const res = await Axios("latest-news")
+    return res.data
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export default async function Home() {
+  const data = await getHomeData()
+
   return (
     <main className="mt-4">
       <div className="container">
-        <h2 className="text-4xl mt-12 mb-4">Hot Topics</h2>
+        <h2 className="text-4xl mt-12 mb-4 font-bold">Hot Topics</h2>
         <div className="flex items-center gap-4 overflow-hidden flex-wrap">
           {HotTopics.map((topic, index) => (
-            <div
+            <Link
+              href={`/category/${topic.slug}`}
               key={index}
               className="bg-slate-50 cursor-pointer rounded-lg h-8 px-4 py-2 flex items-center gap-2 justify-center hover:bg-slate-200 transition-all"
             >
               {topic.icon}
               <p>{topic.name}</p>
-            </div>
+            </Link>
           ))}
         </div>
 
-        <h2 className="text-4xl mt-12 mb-4">Latest News</h2>
+        <h2 className="text-4xl mt-12 mb-4 font-bold">Latest News</h2>
         <div className="grid grid-cols-12 gap-4">
-          <article className="col-span-12 sm:col-span-6 md:col-span-4 overflow-hidden">
-            <Link href="#">
-              <Image
-                alt="news image"
-                src="/imgs/news-1.png"
-                width={400}
-                height={200}
-                className="w-full rounded-lg"
-              />
-              <div className="p-2">
-                <h3 className="text-xl truncate font-bold">
-                  News Title is the best news title ever
-                </h3>
-                <p className="line-clamp-3 text-slate-600">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Tenetur similique eaque saepe recusandae, doloribus,
-                  praesentium voluptas debitis sapiente sequi, minima accusamus?
-                  Corrupti expedita eius commodi illum modi error eaque.
-                  Quisquam?
-                </p>
-                <div className="flex items-center gap-4 mt-4 truncate">
-                  <div className="flex items-center gap-1">
-                    <User className="text-slate-600" />
-                    <p className="text-sm text-slate-600">Mahmoud Dahdouh</p>
+          {data.news.map((item: any) => {
+            return (
+              <article
+                key={item.id}
+                className="col-span-12 sm:col-span-6 md:col-span-4 overflow-hidden"
+              >
+                <Link href={item.url} passHref={true}>
+                  <div>
+                    {item.image !== "None" ? (
+                      <Image
+                        alt="news image"
+                        src={item.image}
+                        width={400}
+                        height={200}
+                        className="w-full rounded-lg h-60 sm:h-48 object-cover shadow-inner"
+                      />
+                    ) : (
+                      <BrokenImage />
+                    )}
+                    <div className="p-2">
+                      <h3 className="text-xl truncate font-bold">
+                        {item.title}
+                      </h3>
+                      <p className="line-clamp-3 text-slate-600">
+                        {item.description}
+                      </p>
+                      <div className="flex items-center gap-4 mt-4 truncate">
+                        <div className="flex items-center gap-1">
+                          <User className="text-slate-600" />
+                          <p className="text-sm text-slate-600">
+                            {item.author}
+                          </p>
+                        </div>
+                        <p className="text-slate-600 text-sm">
+                          {item.published.substring(0, 10)}
+                        </p>
+                      </div>
+                      <div className="flex items-center gap-2 mt-2">
+                        {item.category.map((item: any, index: number) => {
+                          return (
+                            <Link
+                              key={index}
+                              passHref={true}
+                              href={`/category/${item}`}
+                              className="bg-slate-200 hover:bg-slate-400 cursor-pointer transition-all hover:text-white rounded-lg text-sm text-center px-2 py-1 "
+                            >
+                              {item}
+                            </Link>
+                          )
+                        })}
+                      </div>
+                    </div>
                   </div>
-                  <p className="text-slate-600 text-sm">2024-05-05</p>
-                </div>
-                <div className="flex items-center gap-2 mt-2">
-                  <Link href="/asd">
-                    <p className="bg-slate-200 hover:bg-slate-400 cursor-pointer transition-all hover:text-white rounded-lg text-sm text-center px-2 py-1 ">
-                      Hello
-                    </p>
-                  </Link>
-                </div>
-              </div>
-            </Link>
-          </article>
+                </Link>
+              </article>
+            )
+          })}
         </div>
       </div>
     </main>
