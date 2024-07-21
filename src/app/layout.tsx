ff68094136ad2a3294f333c4476e553552109a2a
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2024-07-21 11:22:30 +0300

    loading layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
         <Navbar />
-        {children}
+        <div className="flex-1">{children}</div>
         <Footer />
       </body>
     </html>
